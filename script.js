fc4676fb8c0040ebad1ae70d5d1dde2bf16eee91
Fayik <107407594+fayikefenjyan@users.noreply.github.com>
2026-04-21 19:05:42 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const gifStages = [
-    "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",    // 0 normal
+    "https://media.tenor.com/1QR1IU5NfkcAAAAj/cute-cats.gif",              // 0 normal 
     "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
     "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
-    "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
-    "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
-    "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
-    "https://media.tenor.com/5_tv1HquZlcAAAAj/chiikawa.gif",               // 6 very devastated
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAC/chiikawa-hachiware.gif"  // 7 crying runaway
+    "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad 
+    "https://media.tenor.com/EKrsFntSuSgAAAAj/sad.giff",                   // 4 sadder
+    "https://media.tenor.com/agYucrt9BLgAAAAj/sad-my.gif",                 // 5 devastated
+    "https://media.tenor.com/YoAqt_jAr-UAAAAj/sad-broken-heart.gif",       // 6 very devastated
+    "https://media1.tenor.com/m/uDugCXK4vI4AAAAC/chiikawa-hachiware.gif"   // 7 crying runaway
 ]
 
 const noMessages = [
     "Ոչ",
-    "Դու դրական ես տրամադրված? 🤔",
+    "Դու դրակա՞ն ես տրամադրված 🤔",
     "Խնդրում եմ... 🥺",
     "Եթե դու ասես ոչ, ես կտխրեմ...",
     "Ես շատ կտխրեմ... 😢",
